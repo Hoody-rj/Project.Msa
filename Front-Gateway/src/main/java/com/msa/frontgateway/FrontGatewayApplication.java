7ad54dde2496f99da73e43797156ddbb96bfdbe5
Shaun.the.sheep <robin.Jeong817@gmail.com>
2025-07-31 22:40:09 +0900

    Initial commit

---
+++ Front-Gateway/src/main/java/com/msa/frontgateway/FrontGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.msa.frontgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontGatewayApplication.class, args);
+    }
+
+}
---
+++ Up-Server/src/test/java/com/msa/upserver/UpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msa.upserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
