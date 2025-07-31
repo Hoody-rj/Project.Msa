9fbee58447e7d67d4083d10b383881fab01422c2
Shaun.the.sheep <robin.Jeong817@gmail.com>
2025-07-31 23:14:36 +0900

    2025-07-31
    add : 권환 config 파일 추가
    Middle-Auth로 파일 명이 되어 있는 권한 관련 App 추가
    
    권한 내용
    /auth/signin의 Uri에 대한 접근 허용
    .csrf 비활성화

---
+++ Middle-Auth/src/main/java/com/msa/middleauth/MiddleAuthApplication.java
@@ -0,0 +1,13 @@
+package com.msa.middleauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddleAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiddleAuthApplication.class, args);
+    }
+
+}
---
+++ Middle-Auth/src/test/java/com/msa/middleauth/MiddleAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msa.middleauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiddleAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
