be50fae0d65792a732ae3c6ac7ea7771b177eb63
Shaun.the.sheep <robin.Jeong817@gmail.com>
2025-08-01 10:09:17 +0900

    2025-08-01
    add: 토큰 검증 함수 및 payload 확인 로그 로직 추가

--- Front-Gateway/src/main/java/com/msa/frontgateway/LocalJwtAuthenticationFilter.java
+++ Front-Gateway/src/main/java/com/msa/frontgateway/LocalJwtAuthenticationFilter.java
@@ -1,4 +1,49 @@
 package com.msa.frontgateway;
 
-public class LocalJwtAuthenticationFilter {
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import javax.crypto.SecretKey;
+
+@Slf4j
+public class LocalJwtAuthenticationFilter implements GlobalFilter {
+    @Value("${service.jwt.secret-key}")
+    private String secretKey;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain){
+        String token = extractToken(exchange);
+        return null;
+    }
+
+    private String extractToken(ServerWebExchange exchange){
+        String token = exchange.getRequest().getHeaders().getFirst("Authorization");
+        if (token != null && token.startsWith("Bearer ")) {
+            return token.substring(7);
+        }
+        return null;
+    }
+
+    private boolean validateToken(String token){
+        try{
+            SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+            Jws<Claims> claims = Jwts.parser()
+                    .verifyWith(key)
+                    .build().parseSignedClaims(token);
+            log.info("#####payload : " + claims.getPayload().toString());
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
 }
