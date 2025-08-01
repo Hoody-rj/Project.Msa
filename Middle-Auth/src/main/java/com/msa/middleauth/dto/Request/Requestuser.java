383c18b9e94e1a77e91bd687d0e63c94efa08a74
Shaun.the.sheep <robin.Jeong817@gmail.com>
2025-08-01 17:52:04 +0900

    2025-08-01
    add : user 엔티티 추가 및 Responseuser 추가

--- Middle-Auth/src/main/java/com/msa/middleauth/dto/Request/Requestuser.java
+++ Middle-Auth/src/main/java/com/msa/middleauth/dto/Request/Requestuser.java
@@ -1,4 +1,13 @@
 package com.msa.middleauth.dto.Request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class Requestuser {
+    public String user_id;
+    public String user_pwd;
+    public String user_name;
+    public String user_phone;
 }
